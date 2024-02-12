6d63b1875ab024d7cced9fa3804fa0bfc0abc6bc
SimonaSiug <simonasiugg@gmail.com>
2024-02-12 17:01:45 +0200

    Update darbas.py
    Sukūriau priimti įvestį iš naudotojo

--- darbas.py
+++ darbas.py
@@ -29,3 +29,16 @@ print("2.Atimti")
 print("3.Padauginti")
 print("4.Dalinti")
 
+While True:
+pasirinkimas = input("Irasyti pasirinkima(1/2/3/4):")
+
+if choice in('1','2','3','4'):
+    try:
+        num1 = float(input("iveskite pirma skaiciu:"))
+        num2 = float(input("iveskite antra skaiciu:"))
+        except ValueError:
+            print("Klaida. Prasome ivesti skaiciu.")
+            cintinue
+            
+            if choice == '1':
+                print (num1, "+", num2, "=", add(num1, num2))
