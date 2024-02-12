f3b2dfe7a16b7971c9c11ac23cff414be48b55dc
SimonaSiug <simonasiugg@gmail.com>
2024-02-12 16:32:41 +0200

    Create darbas.py

---
+++ darbas.py
@@ -0,0 +1,12 @@
+kintamasis = 1
+kintamasis = 2
+
+def funkcija ():
+    print(kintamasis)
+    print(kintamasis2)
+    print(f'test:{kintamasis}{kintamasis2}')
+    print(f'test{kintamasis}{kintamasis2}')
+    print('test:{}{}'.format{kintamasis2, kintamasis})
+
+#komentaras
+
