9c50e31e8fd0f929232696db39a130463f1bce15
SimonaSiug <simonasiugg@gmail.com>
2024-02-12 17:48:32 +0200

    Update darbas.py
    Komentarų pridėjimas

--- darbas.py
+++ darbas.py
@@ -10,28 +10,34 @@ def funkcija ():
 
 #komentaras
 
-
+# Dviejų skaičių sudėjimas
 def add (kintamasis1, kintamasis2):
     return kintamasis1 + kintamasis2
 
+# Dviejų skaičių atėmimas
 def subtract (kintamasis1, kintamasis2):
     return kintamasis1 - kintamasis2
 
+# Dviejų skaičių sudauginimas
 def multiply (kintamasis1, kintamasis2):
     return kintamasis1 * kintamasis2
 
+# Dviejų skaičių dalinimas
 def divide (kintamasis1, kintamasis2):
     return kintamasis1 / kintamasis2
 
+# Sukuriama meniu dalis
 print ("Pasirinkite operacija.")
 print("1.Sudeti")
 print("2.Atimti")
 print("3.Sudauginti")
 print("4.Dalinti")
 
+# Naudotojo įvestis priimama
 While True:
 pasirinkimas = input("Irasyti pasirinkima(1/2/3/4):")
 
+# Tikriname ar pasirinkta iš viena iš keturių pasirinkčių
 if pasirinkimas in('1','2','3','4'):
     try:
         num1 = float(input("iveskite pirma skaiciu:"))
