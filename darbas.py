7835b2f0dcb1229a050919c5b298778f7b3844cd
SimonaSiug <simonasiugg@gmail.com>
2024-02-12 16:51:13 +0200

    Update darbas.py

--- darbas.py
+++ darbas.py
@@ -10,3 +10,22 @@ def funkcija ():
 
 #komentaras
 
+
+def add (kintamasis1, kintamasis2):
+    return kintamasis1 + kintamasis2
+
+def subtract (kintamasis1, kintamasis2):
+    return kintamasis1 - kintamasis2
+
+def multiply (kintamasis1, kintamasis2):
+    return kintamasis1 * kintamasis2
+
+def divide (kintamasis1, kintamasis2):
+    return kintamasis1 / kintamasis2
+
+print ("Pasirinkite operacija.")
+print("1.Sudeti")
+print("2.Atimti")
+print("3.Padauginti")
+print("4.Dalinti")
+
